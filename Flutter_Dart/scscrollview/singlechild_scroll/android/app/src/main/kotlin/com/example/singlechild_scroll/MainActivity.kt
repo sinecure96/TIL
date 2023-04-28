cbbcb48aec35b9353c442513a27513b45361d077
sinecure96 <sinecure96@gmail.com>
2023-04-28 17:09:51 +0900

    0428

---
+++ Flutter_Dart/scscrollview/singlechild_scroll/android/app/src/main/kotlin/com/example/singlechild_scroll/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.singlechild_scroll
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Flutter_Dart/scscrollview/singlechild_scroll/lib/screen/home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:singlechild_scroll/layout/main_layout.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MainLayout(
+        title: 'home',
+        body: Column(
+          children: [],
+        ));
+  }
+}
